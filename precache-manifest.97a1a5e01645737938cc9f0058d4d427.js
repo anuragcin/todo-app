bedc05d637fce649162e4a6268960eb0e3f5226d
Anurag <anurag@Tamannas-MacBook-Pro.local>
2019-12-22 18:46:13 -0800

    Updates

---
+++ precache-manifest.97a1a5e01645737938cc9f0058d4d427.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6b7d38ec08ccb299c72",
+    "url": "/todo-app/static/css/main.6158e0c0.chunk.css"
+  },
+  {
+    "revision": "f6b7d38ec08ccb299c72",
+    "url": "/todo-app/static/js/main.f6b7d38e.chunk.js"
+  },
+  {
+    "revision": "64cac5887ee38ce4207f",
+    "url": "/todo-app/static/js/1.64cac588.chunk.js"
+  },
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "a7b84dcdb285393845e267896bfbb8bf",
+    "url": "/todo-app/index.html"
+  }
+];
